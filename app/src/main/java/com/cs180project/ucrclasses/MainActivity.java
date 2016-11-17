71fc9ecb7558111e7d4c4276afde303baad9dcb2
Stavan Thaker <sthak004@ucr.edu>
2016-11-17 15:24:10 -0800

    shrunk calendar image

--- app/src/main/java/com/cs180project/ucrclasses/MainActivity.java
+++ app/src/main/java/com/cs180project/ucrclasses/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Databaser.fetchData();
+        //if(Databaser.fetching)
         UCRSchedules.scheduleInit();
     }
 
